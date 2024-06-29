22724a6772de811d56de53f121e6d79d6f61d3a8
Anesthesiade4cordas <guilhermecqpro@gmail.com>
2024-06-28 22:58:36 -0300

    first commit

---
+++ araymedia.js
@@ -0,0 +1,7 @@
+let notas = [60,90,75,100]
+let media = notas[0]+notas[1]+ notas[2]+notas[3]
+console.log(media/notas.length)
+let media2 = 0
+for(let i =0;i< notas.length;i++){
+media2=media2 + notas[i]
+} console.log(media2/notas.length)
